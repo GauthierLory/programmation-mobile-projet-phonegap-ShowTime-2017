16666356d5f0db62a1872da2687fe056cf89ecd3
GauthierLory <gauthier.axel.lory@gmail.com>
2017-11-21 11:09:00 -0500

    Creation vue + liste

---
+++ app/src/main/assets/www/action/vue/ElementVue.js
@@ -0,0 +1,14 @@
+var ElementVue = function (element)
+{
+	this.afficher = function (element)
+	{
+		var pageListeElement = $("#page-element").html();
+
+		$("body").html(pageElement);
+
+		$("#element-nom").html(element.nom);
+		$("#element-synopsis").html(element.synopsis);
+		$("#element-time").html(element.time);
+		$("#element-date").html(element.date;
+	}
+}
