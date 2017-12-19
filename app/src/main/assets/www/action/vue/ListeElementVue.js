9c3c1e67c0050f03105fad17f3d59f7a51a59f72
lucasmarrel <marrel.lucas@gmail.com>
2017-12-19 18:33:55 -0500

    Modification du slide avec la liste

--- app/src/main/assets/www/action/vue/ListeElementVue.js
+++ app/src/main/assets/www/action/vue/ListeElementVue.js
@@ -5,6 +5,31 @@ var ListeElementVue = function(listeElement)
 
         $("#contenu").html(ListeElementVue.pageListeElement);
 
+        var htmlSlideIndice = $("#slide-indice");
+        var slideIndice = "";
+        for (var indiceElement in listeElement)
+            {
+                slideIndice += '<li data-target="#carouselExampleIndicators" data-slide-to="'+ listeElement[indiceElement].id +'"></li>';
+
+            }
+        htmlSlideIndice.html(slideIndice);
+
+        var htmlSlideAffiche = $("#slide-affiches");
+        var slideAffiche = "";
+        var actif = 1;
+        for (var indiceElement in listeElement)
+            {
+                if (actif == 1){
+                    slideAffiche += '<div class="carousel-item active"><img class="rounded mx-auto d-block img-fluid" src="images/affiches/'+listeElement[indiceElement].nom+'.jpg" alt="First slide"><div class="carousel-caption d-none d-md-block"><a class="btn btn-success animated rubberBand" href="#modifier-element/' +listeElement[indiceElement].id + '">Modifier</a></div></div>';
+                    var actif = 0;
+                }else{
+                    slideAffiche += '<div class="carousel-item"><img class="rounded mx-auto d-block img-fluid" src="images/affiches/'+listeElement[indiceElement].nom+'.jpg" alt="First slide"><div class="carousel-caption d-none d-md-block"><a class="btn btn-success animated rubberBand" href="#modifier-element/' +listeElement[indiceElement].id + '">Modifier</a></div></div>';
+
+                }
+
+            }
+        htmlSlideAffiche.html(slideAffiche);
+
         var htmlListeElement = $("#liste-element");
         var li = "";
         for (var indiceElement in listeElement)
