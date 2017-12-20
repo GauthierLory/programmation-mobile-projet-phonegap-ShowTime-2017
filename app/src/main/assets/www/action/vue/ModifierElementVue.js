d882ee53a0669d416bc00f9067141af581aaabd9
GauthierLory <gauthier.axel.lory@gmail.com>
2017-12-20 23:39:55 +0100

    modification de la Vue midifier

--- app/src/main/assets/www/action/vue/ModifierElementVue.js
+++ app/src/main/assets/www/action/vue/ModifierElementVue.js
@@ -13,18 +13,23 @@ var ModifierElementVue = function(element)
         var elementSynopsis = '<label for="synopsis">synopsis</mark></label><input type="textarea" name="synopsis" id="synopsis" value ="'+  element.synopsis +'"/>';
         $("#synopsis").html(elementSynopsis);
 
-        var elementTime = '<label for="time">Time</label><textarea name="time" id="time" value ="'+ element.time +'"/>';
-        $("#time").html(elementTime);
+        var elementPhoto = '<label for="photo">Photo</label><textarea name="photo" id="photo" valute ="'+ element.photo+'"/>';
+        $("#photo").html(elementPhoto);
 
         var elementDate = '<label for="date">date</label><input type="date" name="date" id="date" value ="'+ element.date +'"/>';
-        $("#taille").html(elementDate);
+        $("#date").html(elementDate);
 
         var elementCategorie = '<label for="categorie">categorie</label><input type="text" name="categorie" id="categorie" value ="'+ element.categorie +'"/>';
-        $("#reference").html(elementCategorie);
+        $("#categorie").html(elementCategorie);
 
         var elementGenre = '<label for="genre">Genre</label><input type="text" name="genre" id="genre" value ="'+ element.genre +'"/>';
-        $("#prix").html(elementGenre);
+        $("#genre").html(elementGenre);
 
+        var elementEmail = '<label for="email">Email</label><textarea name="Email" id="email" value ="'+ element.email +'"/>';
+        $("#email").html(elementEmail);
+
+        var elementNote = '<label for="note">Note</label><textarea name="note" id="note" value ="'+ element.note +'"/>';
+        $("#note").html(elementNote);
 
 
              $("#formulaire-ajouter").on("submit", function (evenement)
@@ -32,12 +37,14 @@ var ModifierElementVue = function(element)
             evenement.preventDefault();
             var nom = $("#nom").val();
             var synopsis = $("#synopsis").val();
-            var time = $("#time").val();
+            var photo = $("#photo").val();
             var date = $("#date").val();
             var categorie = $("#categorie").val();
             var genre = $("#genre").val();
+            var email = $("#email").val();
+            var note = $("#note").val();
 
-            instance.element = new element(nom, synopsis, time, date, categorie, genre)
+            instance.element = new element(nom, synopsis, photo, date, categorie, genre, emqil,note)
             window.location.hash = "#AjouterElementVue:NouveauElement";
         });
     }
