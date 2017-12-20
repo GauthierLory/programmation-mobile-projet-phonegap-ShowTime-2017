b16dc9ef30f2c79fd17bef05463d3bfaf6c94781
GauthierLory <gauthier.axel.lory@gmail.com>
2017-12-20 03:43:40 -0500

    préparation du Local Storage, manque intégration

---
+++ app/src/main/assets/www/action/donnees/ElementDAOLocalStorage.js
@@ -0,0 +1,80 @@
+var ElementDAOLocalStorage = function()
+{
+    var element1 = new Element(1,"Interstellar","Le film raconte les aventures d’un groupe d’explorateurs qui utilisent une faille récemment découverte dans l’espace-temps afin de repousser les limites humaines et partir à la conquête des distances astronomiques dans un voyage interstellaire. ","2h08","21/12/17","Film","Action");
+
+    var element2 = new Element(2,"Mission Impossible - Protocole Fantôme","Impliquée dans l'attentat terroriste du Kremlin, l'agence Mission Impossible (IMF) est totalement discréditée. Tandis que le président lance l'opération Protocole Fantôme, Ethan Hunt, privé de ressources et de renfort, doit trouver le moyen de blanchir l'agence et de déjouer toute nouvelle tentative d'attentat. Mais pour compliquer encore la situation, l'agent doit s'engager dans cette mission avec une équipe de fugitifs d'IMF dont il n'a pas bien cerné les motivations…","2h08","21/12/17","Serie","Horreur");
+
+    var element3 = new Element(3,"Skyfall","Lorsque la dernière mission de Bond tourne mal, plusieurs agents infiltrés se retrouvent exposés dans le monde entier. Le MI6 est attaqué, et M est obligée de relocaliser l’Agence. Ces événements ébranlent son autorité, et elle est remise en cause par Mallory, le nouveau président de l’ISC, le comité chargé du renseignement et de la sécurité. Le MI6 est à présent sous le coup d’une double menace, intérieure et extérieure. Il ne reste à M qu’un seul allié de confiance vers qui se tourner : Bond. Plus que jamais, 007 va devoir agir dans l’ombre. Avec l’aide d’Eve, un agent de terrain, il se lance sur la piste du mystérieux Silva, dont il doit identifier coûte que coûte l’objectif secret et mortel…","2h08","21/12/17","Serie","Horreur");
+
+    listeElement = null;
+
+      initialiser = function (){
+	        if (!listeElement && localStorage['element'])
+	        {
+	            listeElement = new Array();
+	            listeElement = JSON.parse(localStorage['element']);
+	            for(var indiceAnonyme in listeAnonyme)
+	            {
+	                elementAnonyme = listeAnonyme[indiceAnonyme];
+	                element = new Element(elementAnonyme.nom,
+	                                    elementAnonyme.synopsis,
+	                                    elementAnonyme.photo,
+	                                    elementAnonyme.date,
+	                                    elementAnonyme.categorie,
+	                                    elementAnonyme.genre,
+	                                    elementAnonyme.email,
+	                                    elementAnonyme.note,
+	                                    elementAnonyme.id);
+	                listeElement.push(element);
+	            }
+	        }
+	        if (!listeElement)
+	        {
+	            listeElement = new Array();
+	        }
+	    }
+
+  	this.getListeElement = function(){
+        return listeElement;
+    }
+
+    trouverNouvelId = function (){
+        maximum = 0;
+        for (var indiceElement in listeElement)
+        {
+            element = listeElement[indiceElement];
+            if (element.id > maximum)
+                maximum = element.id;
+        }
+        return maximum + 1;
+    }
+
+    this.ajouterElement = function (element){
+        element.id = trouverNouvelId();
+        listeElement.push(element);
+        localStorage['element'] = JSON.stringify(listeElement);
+    }
+
+    this.getElementParId = function (id){
+        for (var indiceElement in listeElement)
+        {
+            element = listeElement[indiceElement];
+            if (element.id == id) return element;
+        }
+    }
+
+    this.modifierElement = function(elementModifier){
+        for (var indiceElement in listeElement){
+
+            element = listeElement[indiceElement];
+            if(element.id == elementModifier.id){
+
+                listeElement[indiceElement] = elementModifier;
+       			localStorage['element'] = JSON.stringify(listeElement);
+                return;
+            }
+        }
+    }
+
+initialiser();
+}
