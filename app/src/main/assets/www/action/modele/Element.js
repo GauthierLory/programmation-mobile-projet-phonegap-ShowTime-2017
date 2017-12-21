c927c969777aa8d08fd5b99c0777b5058ab1335b
lucasmarrel <marrel.lucas@gmail.com>
2017-12-20 19:35:06 -0500

    Ajout du champs URL

--- app/src/main/assets/www/action/modele/Element.js
+++ app/src/main/assets/www/action/modele/Element.js
@@ -1,10 +1,10 @@
-var Element = function (id = undefined, nom, synopsis, duree, date, categorie, genre, photo, email, note)
+var Element = function (id = undefined, nom, synopsis, duree, date, categorie, genre, url, email, note)
 {
 	this.id = id;
 	this.nom = nom;
 	this.synopsis = synopsis;
 	this.duree = duree;
-	this.photo = photo;
+	this.url = url;
 	this.date = date;
 	this.categorie = categorie;
 	this.genre = genre;
--- app/src/main/assets/www/action/vue/AjouterElementVue.js
+++ app/src/main/assets/www/action/vue/AjouterElementVue.js
@@ -19,6 +19,8 @@ var AjouterElementVue = function()
             console.log("EMAIL : "+email);
             var date = $("#date").val();
             console.log("DATE : "+date);
+            var url = $("#url").val();
+            console.log("URL : "+url);
             if (document.getElementById('c1').checked) {
               var categorie = document.getElementById('c1').value;
             }else if (document.getElementById('c2').checked){
@@ -29,7 +31,7 @@ var AjouterElementVue = function()
             var genre = $("#genre").val();
             console.log("GENRE : "+genre);
 
-            instance.element = new Element(0,titre, synopsis, "2:04", date, categorie, genre, "photo.jpg", email);
+            instance.element = new Element(0,titre, synopsis, "2:04", date, categorie, genre, url, email);
 //            instance.element = new Element(0,"test", "test", "2:99", "12/12", "serie", "comedie");
             window.location.hash = "#AjouterElementVue:NouveauElement";
         });
